0ee5b43c5ca279cc05b1806b08cf6d931157f838
ZsomborHajzer <114413262+ZsomborHajzer@users.noreply.github.com>
2023-04-30 10:57:17 +0100

    Set Up MERN-Stack skeleton
    Set up a mern stack on our repository, this is not final this is just an outline based on someone elses project. This can be changed as much as you would like.

---
+++ server/app.js
@@ -0,0 +1,38 @@
+//import modules
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+require("dotenv").config();
+const fs = require('fs');
+const path = require('path');
+
+
+
+
+// app
+const app = express();
+
+
+// db
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("DB connected!")).catch(err => console.log('DB Connection Error', err));
+
+
+// middleware
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentrials: true }));
+
+// routes
+
+
+
+//port
+const port = process.env.PORT || 3000;
+
+
+
+// listener
+const server = app.listen(port, () => console.log(`server is running on port ${port}`));
