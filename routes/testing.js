ec57d49b2ff6414776e7ddbed8610cc634aac0a6
NidLip <nikolay.minins@student.nhlstenden.com>
2023-05-25 16:39:25 +0200

    Completed connection with openAI

--- routes/testing.js
+++ routes/testing.js
@@ -1,53 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration,  OpenAIApi } = require('openai');
+let openai;
+
 require("dotenv").config();
 
-
 // openAI configuration
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-var rl = require('readline-sync');
-
+if (process.env.OPENAI_API_KEY) {
+    const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+    openai = new OpenAIApi(configuration);
+}
 //import controllers
 const { getTesting } = require('../controllers/testing');
 
-//import middlewares
-const isAuth = require('../middleware/is-auth');
-
-
-
 // api routes
-router.get('/', isAuth, getTesting)
+router.get('/', getTesting);
 
 module.exports = router;
 
+// Replace "instructions" with your desired instructions
+const instructions = "You are an AI teacher. Answer the questions as clearly and concisely as possible.";
+
 // back-end testing (function for AI) //
-console.log(process.env.OPENAI_API_KEY);
 async function startAssistant() {
-    var messages = [];
+    let response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "You are a teacher, write down a test question about HTML and CSS with multiple answers to it, but not say which one is correct.",
+        "max_tokens": 60,
+        "temperature": 1,
+    });
 
-    var input = rl.question('What type of chatbot would you like to create? > ');
-    messages.push({ "role": "system", "content": "Stay in character. You are " + input });
+    let response_text = response.data.choices[0].text;
+    console.log(response_text);
 
-    input = rl.question("Say hello to your new chatbot > ");
-    while (true) {
-        messages.push({ "role": "user", "content": input });
 
-        console.log("...");
-        const completion = await openai.createChatCompletion({
-            model: "gpt-4",
-            messages: messages,
-        });
-
-        var reply = completion.data.choices[0].message.content;
-        messages.push({ "role": "assistant", "content": reply });
-        console.log(reply);
-        input = rl.question("> ");
-    }
 }
+
+// Call the startAssistant function
 
 startAssistant();
