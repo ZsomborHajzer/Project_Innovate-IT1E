4e4f667cbe7b659896d13978e533e4ad23385838
ZsomborHajzer <114413262+ZsomborHajzer@users.noreply.github.com>
2023-06-09 11:49:58 +0100

    Added backend RestAPI endpoints for  assignements

--- controllers/assignments.js
+++ controllers/assignments.js
@@ -1,22 +1,41 @@
+//initialize express router
+const express = require('express');
+const { db } = require('../models/user');
+const router = express.Router();
 
-
+//import DB 
+const Assignments = db.collection('Assignments')
 
 exports.getAssigment = async (req, res) => {
-
-
-    res.status(200).json({
-        message: 'Assigment page is working',
-    })
+    const chosenTopic = req.body.topic;
+    const assignmentObj = await Assignments.findOne({ topic: chosenTopic });
+    console.log(assignmentObj.questions);
+    res.status(200).json({ assignments: assignmentObj.questions });
 };
 
 exports.getSpecificAssigment = async (req, res) => {
-    res.status(200).json({
-        message: 'Specific assigment page is working',
-    })
-}
+    const topic = req.body.topic;
+    const questionNum = req.body.questionNum;
+    const questionObj = await Assignments.findOne({ topic: topic });
+
+    for (let i = 0; i < questionObj.questions.length; i++) {
+        if (questionObj.questions[i].questionNum === questionNum) {
+            console.log(questionObj.questions[i].question);
+            res.status(200).json({ question: questionObj.questions[i] });
+            return;
+        }
+    }
+    res.status(404).json({ message: "Question not found" });
+};
 
 exports.getNumberOfQuestions = async (req, res) => {
-    res.status(200).json({
-        message: 'Number of questions page is working',
-    })
+    const PHPObj = await Assignments.findOne({ topic: "PHP" });
+    const JAVAObj = await Assignments.findOne({ topic: "JAVA" });
+    const HTMLObj = await Assignments.findOne({ topic: "HTML/CSS" });
+
+    const PHPNum = PHPObj.questions.length;
+    const JAVANum = JAVAObj.questions.length;
+    const HTMLNum = HTMLObj.questions.length;
+
+    res.status(200).json({ PHPNum: PHPNum, JAVANum: JAVANum, HTMLNum: HTMLNum });
 };
