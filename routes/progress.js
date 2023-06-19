117a4108511fd6a473d863fbe60c881719648399
ZsomborHajzer <114413262+ZsomborHajzer@users.noreply.github.com>
2023-06-19 13:15:33 +0100

    minor code formatting before merge

--- routes/progress.js
+++ routes/progress.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 //import controllers
-console.log('Hey HEY HEY');
 const { getProgress } = require('../controllers/progress');
-console.log('AYAYAYAYA');
+
 //import middlewares
 const isAuth = require('../middleware/is-auth');
 
